ce36aeb36ddda5f2ea50f8e436e49f8c8b4470bd
khairnarswapna <khairnarswapna99@gmail.com>
2019-11-26 18:20:13 +0530

    refactored code with avoid unwated line

--- src/main/java/com/moodanalyser/MoodAnalysisException.java
+++ src/main/java/com/moodanalyser/MoodAnalysisException.java
@@ -2,10 +2,10 @@ package com.moodanalyser;
 
 public class MoodAnalysisException extends Exception {
     public enum ExceptionType{
+
         ENTERED_NULL, ENTERD_EMPTY , NO_SUCH_FIELD , NO_SUCH_METHOD,
     }
     public ExceptionType type;
-
     public MoodAnalysisException(ExceptionType type, String message) {
         super(message);
         this.type=type;
